4e0ebba776ce4288c447654337fbc812fb0a0fef
Zhanbek Utaraliev <98697409+ZhanbUuu@users.noreply.github.com>
2022-08-25 12:34:05 +0300

    Update pars3.py

--- pars3.py
+++ pars3.py
@@ -7,6 +7,7 @@ headers = {
     "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
 }
 
+# the function takes the URL of the image as input and writes it to the specified folder
 def download_img(url):
     resp = requests.get(url, stream=True)
     r = open("C:\\Users\\User\\Desktop\\image\\" + url.split("/")[-1], "wb")
@@ -14,17 +15,18 @@ def download_img(url):
         r.write(v)
     r.close()
 
-
+# generator function outputs each product page
 def get_url():
     for i in range(1, 8):
         url = "https://scrapingclub.com/exercise/list_basic/?page=" + str(i)
-
         src = requests.get(url, headers=headers)
         soup = BeautifulSoup(src.text, "lxml")
         card_links = soup.find_all("h4", class_="card-title")
         for value in card_links:
             links = "https://scrapingclub.com" + value.find("a").get("href")
             yield links
+            
+# the generator function takes as input a link for each category page and outputs everything about the product
 def get_data():
     for links in get_url():
         src = requests.get(links, headers=headers)
